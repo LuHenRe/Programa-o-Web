df20163b40bbb1422c7d7662a20939b22b806274
LuHenRe <154239376+LuHenRe@users.noreply.github.com>
2026-08-18 15:48:15 -0300

    Update email.py

--- backend/app/domain/value_objects/email.py
+++ backend/app/domain/value_objects/email.py
@@ -2,12 +2,12 @@ import re
 
 class Email:
     def __init__(self, value: str) -> None:
-        if not self._is_valid():
+        if not self._is_valid(value):
             raise ValueError("Email inválido")
         self.value = value
 
     def _is_valid(self, email: str) -> bool:
-        pattern = r"^[\\w\\.-]+@[\\w\\.-]+\\.\\w+$"
+        pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
         return re.match(pattern, email) is not None
 
     def _eq_(self, other) -> bool:
